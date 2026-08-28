068701a2dc9dfb8b193f8d8f64d574293177cb6d
yeooon030 <cuoo030@naver.com>
2026-08-29 00:30:59 +0900

    chore: Spring Boot 프로젝트 초기화

---
+++ backend/src/main/java/com/orderflow/OrderFlowApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/orderflow/OrderFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
